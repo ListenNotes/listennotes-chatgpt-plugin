59ed89005b4635eadbba8d233ecb66c3d5ea5f4c
Wenbin Fang <wenbin@listennotes.com>
2023-06-14 17:27:51 -0700

    Update

--- edge-src/common/ListenApiManager.js
+++ edge-src/common/ListenApiManager.js
@@ -4,16 +4,19 @@ const { ClientForWorkers } = require('podcast-api');
 
 export default class ListenApiManager {
   constructor(context, EndpointDef) {
-    const {env} = context
+    const {env, data} = context
+
     this.client = ClientForWorkers({
       apiKey: env.LISTEN_API_KEY || null,
     })
+
+    this.data = data
     this.endpointDef = new EndpointDef()
     this.responseBuilder = new ResponseBuilder(context)
   }
 
   async getResponse() {
-    const res = await this.client[this.endpointDef.apiFunctionName()]()
+    const res = await this.client[this.endpointDef.apiFunctionName()](this.data.searchParams)
     return this.responseBuilder.getJsonResponse(
       this.endpointDef.transformResultFunc(res.data))
   }
--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,9 @@
 async function preprocessRequest({request, next, data}) {
   const urlObj = new URL(request.url)
+
   data.baseUrl = urlObj.origin
+  data.searchParams = Object.fromEntries(urlObj.searchParams)
+
   return next()
 }
 
