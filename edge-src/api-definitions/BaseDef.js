6641893d4736e1e1af7f80835ff5175e6255ba58
Wenbin Fang <wenbin@listennotes.com>
2023-06-14 17:15:37 -0700

    Update

--- edge-src/api-definitions/BaseDef.js
+++ edge-src/api-definitions/BaseDef.js
@@ -1,7 +1,72 @@
+export const OPENAPI_PROPERTIES = {
+  episodeSpec: {
+    title: {
+      type: 'string',
+      description: 'title of the podcast episode',
+    },
+    description: {
+      type: 'string',
+      description: 'description of the podcast episode in html',
+    },
+    image: {
+      type: 'string',
+      description: 'image url of the podcast episode',
+    },
+    audio: {
+      type: 'string',
+      description: 'audio url of the podcast episode, used for playing the audio',
+    },
+    audio_length_sec: {
+      type: 'integer',
+      description: 'audio length of the podcast episode in seconds',
+    },
+    pub_date_ms: {
+      type: 'integer',
+      description: 'published date of the podcast episode in milliseconds of the Unix epoch',
+    },
+    listennotes_url: {
+      type: 'string',
+      description: 'the canonical url of the podcast episode on Listen Notes, ' +
+        'which can be used to manually share this episode',
+    },
+  },
+  podcastSpec: {
+    title: {
+      type: 'string',
+      description: 'title of the podcast that this episode belongs to',
+    },
+    publisher: {
+      type: 'string',
+      description: 'publisher of the podcast that this episode belongs to',
+    },
+    image: {
+      type: 'string',
+      description: 'image url of the podcast that this episode belongs to',
+    },
+    listen_score: {
+      type: 'integer',
+      description: 'Listen Score of the podcast that this episode belongs to, ' +
+        'which indicates the estimated popularity of the podcast (similar to nielsen ratings)',
+    },
+    listen_score_global_rank: {
+      type: 'string',
+      description: 'the global rank of the podcast according to Listen Score',
+    },
+    listennotes_url: {
+      type: 'string',
+      description: 'the canonical url of the podcast on Listen Notes, ' +
+        'which can be used to manually share this podcast',
+    },
+  },
+}
+
 export default class BaseDef {
   constructor() {
   }
 
+  //
+  // Implement these functions
+  //
   transformResultFunc(result) {
     return {}
   }
@@ -12,5 +77,28 @@ export default class BaseDef {
 
   openApiPathSpec() {
     return {}
+  }
+
+  //
+  // Internal utility functions
+  //
+  _makeOpenApiPathSpec(params) {
+    return ({
+      get: {
+        operationId: params.operationId,
+        description: params.description,
+        parameters: [...params.parameters],
+        responses: {
+          '200': {
+            description: params.response200.description,
+            content: {
+              'application/json': {
+                schema: params.response200.schema,
+              },
+            },
+          },
+        },
+      },
+    })
   }
 }
--- edge-src/api-definitions/JustListenDef.js
+++ edge-src/api-definitions/JustListenDef.js
@@ -1,4 +1,4 @@
-import BaseDef from "./BaseDef";
+import BaseDef, {OPENAPI_PROPERTIES} from "./BaseDef";
 
 export default class JustListenDef  extends BaseDef {
   apiFunctionName() {
@@ -26,83 +26,34 @@ export default class JustListenDef  extends BaseDef {
   }
 
   openApiPathSpec() {
-    const justListenEndpoint = {
-      get: {
-        operationId: 'justListen',
-        description: 'Get a random podcast episode, ' +
-          'with all necessary metadata to describe this episode and stream the audio.' +
-          'Recently published episodes are more likely to be fetched.',
-        parameters: [],
-        responses: {
-          '200': {
-            description: 'Returns a json object with the podcast episode data',
-            content: {
-              'application/json': {
-                schema: {
-                  type: 'object',
-                  properties: {
-                    title: {
-                      type: 'string',
-                      description: 'title of the podcast episode',
-                    },
-                    description: {
-                      type: 'string',
-                      description: 'description of the podcast episode in html',
-                    },
-                    image: {
-                      type: 'string',
-                      description: 'image url of the podcast episode',
-                    },
-                    audio: {
-                      type: 'string',
-                      description: 'audio url of the podcast episode, used for playing the audio',
-                    },
-                    audio_length_sec: {
-                      type: 'integer',
-                      description: 'audio length of the podcast episode in seconds',
-                    },
-                    pub_date_ms: {
-                      type: 'integer',
-                      description: 'published date of the podcast episode in milliseconds of the Unix epoch',
-                    },
-                    listennotes_url: {
-                      type: 'string',
-                      description: 'the canonical url of the podcast episode on Listen Notes, ' +
-                        'which can be used to manually share this episode',
-                    },
-                    podcast: {
-                      type: 'object',
-                      properties: {
-                        title: {
-                          type: 'string',
-                          description: 'title of the podcast that this episode belongs to',
-                        },
-                        publisher: {
-                          type: 'string',
-                          description: 'publisher of the podcast that this episode belongs to',
-                        },
-                        image: {
-                          type: 'string',
-                          description: 'image url of the podcast that this episode belongs to',
-                        },
-                        listen_score: {
-                          type: 'integer',
-                          description: 'Listen Score of the podcast that this episode belongs to, ' +
-                            'which indicates the estimated popularity of the podcast (similar to nielsen ratings)',
-                        },
-                        listen_score_global_rank: {
-                          type: 'string',
-                          description: 'the global rank of the podcast according to Listen Score',
-                        },
-                        listennotes_url: {
-                          type: 'string',
-                          description: 'the canonical url of the podcast on Listen Notes, ' +
-                            'which can be used to manually share this podcast',
-                        },
-                      },
-                    },
-                  },
-                },
+    const {episodeSpec, podcastSpec} = OPENAPI_PROPERTIES;
+    const params = {
+      operationId: 'justListen',
+      description: 'Get a random podcast episode, ' +
+        'with all necessary metadata to describe this episode and stream the audio.' +
+        'Recently published episodes are more likely to be fetched.',
+      parameters: [],
+      response200: {
+        description: 'Returns a json object with the podcast episode data',
+        schema: {
+          type: 'object',
+          properties: {
+            title: {...episodeSpec.title},
+            description: {...episodeSpec.description},
+            image: {...episodeSpec.image},
+            audio: {...episodeSpec.audio},
+            audio_length_sec: {...episodeSpec.audio_length_sec},
+            pub_date_ms: {...episodeSpec.pub_date_ms},
+            listennotes_url: {...episodeSpec.listennotes_url},
+            podcast: {
+              type: 'object',
+              properties: {
+                title: {...podcastSpec.title},
+                publisher: {...podcastSpec.publisher},
+                image: {...podcastSpec.image},
+                listen_score: {...podcastSpec.listen_score},
+                listen_score_global_rank: {...podcastSpec.listen_score_global_rank},
+                listennotes_url: {...podcastSpec.listennotes_url},
               },
             },
           },
@@ -110,7 +61,7 @@ export default class JustListenDef  extends BaseDef {
       },
     }
     return {
-      '/just_listen': justListenEndpoint,
+      '/just_listen': this._makeOpenApiPathSpec(params),
     }
   }
 }
